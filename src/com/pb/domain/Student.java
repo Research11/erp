fdf91dbbe94ace13707152e1d4a10d5da16eeedc
Research11 <345303227@qq.com>
2016-04-29 23:47:00 +0800

    brh update

--- src/com/pb/domain/Student.java
+++ src/com/pb/domain/Student.java
@@ -8,5 +8,37 @@ public class Student {
   String email;
   Integer age;
   Date birthday;
+public Integer getId() {
+	return id;
+}
+public void setId(Integer id) {
+	this.id = id;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getEmail() {
+	return email;
+}
+public void setEmail(String email) {
+	this.email = email;
+}
+public Integer getAge() {
+	return age;
+}
+public void setAge(Integer age) {
+	this.age = age;
+}
+public Date getBirthday() {
+	return birthday;
+}
+public void setBirthday(Date birthday) {
+	this.birthday = birthday;
+}
+  
+  
   
 }
